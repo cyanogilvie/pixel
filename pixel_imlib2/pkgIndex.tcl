a5922b908bc71ba8b7585cf1d4d087dcffefdedc
root <root>
2003-10-20 15:39:42 +0000

    Backed out bogus checkin

--- pixel_imlib2/pkgIndex.tcl
+++ pixel_imlib2/pkgIndex.tcl
@@ -1,5 +1,5 @@
 #Tcl package index file, version 1.0
 
-package ifneeded Pixel_magick 1.0 [list eval [list namespace eval ::pixel::magick [list variable library $dir]]\n[list source [file join $dir pixel_magick.tcl]]]
+package ifneeded Pixel_imlib2 1.0 [list eval [list namespace eval ::pixel::imlib2 [list variable library $dir]]\n[list source [file join $dir pixel_imlib2.tcl]]]
 
 
--- pixel_imlib2/test.tcl
+++ pixel_imlib2/test.tcl
@@ -4,7 +4,7 @@ set auto_path	[concat . $auto_path]
 
 package require ML
 package require Pixel_sdl
-package require Pixel_magick
+package require Pixel_imlib2
 
 set xres	800
 set yres	600
@@ -14,15 +14,14 @@ proc progress {percent} {
 }
 
 set scr	[pixel::sdl::setup_screen $xres $yres 32]
-set bg	[pixel::magick::load_image [file join /src ucss lyrics images "default_blue.jpg"] progress]
-#set bg	[pixel::magick::load_image [file join .. "bg.tiff"]]
-#set scaled	[pixel::magick::scale_pmap $bg $xres $yres 0 100 100 100 100 progress]
-set scaled	[pixel::magick::scale_pmap $bg $xres $yres]
-#pixel::pmap_paste $scr $bg 0 0 0
+set bg	[pixel::imlib2::load_image [file join .. "bg.tiff"] progress]
+#set bg	[pixel::imlib2::load_image [file join .. "bg.tiff"]]
+set scaled	[pixel::imlib2::scale_pmap $bg $xres $yres 0 100 100 100 100 progress]
+#set scaled	[pixel::imlib2::scale_pmap $bg $xres $yres]
 pixel::pmap_paste $scr $scaled 0 0 0
 
-#pixel::magick::save_image $scr "test_save.jpg" "jpg" progress
-#pixel::magick::save_image $scr "test_save.png"
+pixel::imlib2::save_image $scr "test_save.jpg" "jpg" progress
+#pixel::imlib2::save_image $scr "test_save.png"
 
 proc expose {evname sdl_event} {
 	pixel::sdl::do_frame $::scr
