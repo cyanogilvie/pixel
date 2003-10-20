b78e292b11244e649d53fc1deb2b7f8d76e0ddfb
root <root>
2003-10-20 15:21:50 +0000

    ImageMagick submodule

--- pixel_imlib2/main.c
+++ pixel_imlib2/main.c
@@ -1,13 +1,21 @@
 // vim: ts=4 shiftwidth=4 tags=../tags
 
+#ifdef DEBUG
+#define DBG(format, args...) fprintf(stderr, "D: %s:%u:%s() " format, \
+		                  __FILE__, __LINE__, __FUNCTION__ , ## args)
+#else
+#define DBG(format , args...)
+#endif
+
 #include <X11/Xlib.h>
-#include <Imlib2.h>
 #include <tcl.h>
 #include <tclstuff.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <strings.h>
 #include <string.h>
+#include <magick/api.h>
+#include <magick/error.h>
 
 #include "2d.h"
 #include "tcl_pmap.h"
@@ -15,7 +23,10 @@
 
 static Tcl_Interp *g_interp;
 static Tcl_Obj *g_progress_cb = NULL;
+static ExceptionInfo	g_exception;
+static ImageInfo		*g_imageinfo;
 
+/*
 static int progress_func(Imlib_Image image, char percent, //{{{1
 		int update_x, int update_y, int update_w, int update_h)
 {
@@ -35,117 +46,13 @@ static int progress_func(Imlib_Image image, char percent, //{{{1
 	}
 	return 1;
 }
-
-
-static char *lookup_load_error(Imlib_Load_Error error) //{{{1
-{
-	switch (error) {
-		case IMLIB_LOAD_ERROR_NONE:
-			return "ok";
-		case IMLIB_LOAD_ERROR_FILE_DOES_NOT_EXIST:
-		case IMLIB_LOAD_ERROR_PATH_COMPONENT_NON_EXISTANT:
-		case IMLIB_LOAD_ERROR_PATH_COMPONENT_NOT_DIRECTORY:
-		case IMLIB_LOAD_ERROR_PATH_POINTS_OUTSIDE_ADDRESS_SPACE:
-			return "File not found";
-		case IMLIB_LOAD_ERROR_FILE_IS_DIRECTORY:
-			return "File is a directory";
-		case IMLIB_LOAD_ERROR_PERMISSION_DENIED_TO_READ:
-			return "Permission denied reading file";
-		case IMLIB_LOAD_ERROR_NO_LOADER_FOR_FILE_FORMAT:
-			return "Image format not recognised";
-		case IMLIB_LOAD_ERROR_PATH_TOO_LONG:
-			return "Filename is too long";
-		case IMLIB_LOAD_ERROR_TOO_MANY_SYMBOLIC_LINKS:
-			return "Too many symbolic links";
-		case IMLIB_LOAD_ERROR_OUT_OF_MEMORY:
-			return "Out of memory";
-		case IMLIB_LOAD_ERROR_OUT_OF_FILE_DESCRIPTORS:
-			return "Out of file descriptors";
-		case IMLIB_LOAD_ERROR_PERMISSION_DENIED_TO_WRITE:
-			return "Permission denied writing to file";
-		case IMLIB_LOAD_ERROR_OUT_OF_DISK_SPACE:
-			return "Out of disk space";
-		default:
-		case IMLIB_LOAD_ERROR_UNKNOWN:
-			return "Unknown error";
-	}
-}
-
-
-static int glue_set_cache_size(ClientData foo, Tcl_Interp *interp, //{{{1
-		int objc, Tcl_Obj *CONST objv[])
-{
-	int cache_size;
-	
-	CHECK_ARGS(1, "cache_size");
-
-	TEST_OK(Tcl_GetIntFromObj(interp, objv[1], &cache_size));
-	imlib_set_cache_size(cache_size);
-	
-	return TCL_OK;
-}
-
-
-static int glue_set_font_cache_size(ClientData foo, Tcl_Interp *interp, //{{{1
-		int objc, Tcl_Obj *CONST objv[])
-{
-	int cache_size;
-	
-	CHECK_ARGS(1, "cache_size");
-
-	TEST_OK(Tcl_GetIntFromObj(interp, objv[1], &cache_size));
-	imlib_set_font_cache_size(cache_size);
-	
-	return TCL_OK;
-}
-
-
-static int glue_add_path_to_font_path(ClientData foo, Tcl_Interp *interp, //{{{1
-		int objc, Tcl_Obj *CONST objv[])
-{
-	CHECK_ARGS(1, "path");
-
-	imlib_add_path_to_font_path(Tcl_GetString(objv[1]));
-	
-	return TCL_OK;
-}
-
-
-static int glue_set_color_usage(ClientData foo, Tcl_Interp *interp, //{{{1
-		int objc, Tcl_Obj *CONST objv[])
-{
-	int num_colours;
-	
-	CHECK_ARGS(1, "num_colours");
-
-	TEST_OK(Tcl_GetIntFromObj(interp, objv[1], &num_colours));
-	
-	imlib_set_color_usage(num_colours);
-	
-	return TCL_OK;
-}
-
-
-static int glue_context_set_dither(ClientData foo, Tcl_Interp *interp, //{{{1
-		int objc, Tcl_Obj *CONST objv[])
-{
-	int dither;
-	
-	CHECK_ARGS(1, "dither?");
-
-	TEST_OK(Tcl_GetBooleanFromObj(interp, objv[1], &dither));
-	
-	imlib_context_set_dither(dither);
-	
-	return TCL_OK;
-}
+*/
 
 
 static int glue_load_image(ClientData foo, Tcl_Interp *interp, //{{{1
 		int objc, Tcl_Obj *CONST objv[])
 {
-	Imlib_Image			image;
-	Imlib_Load_Error	error;
+	Image				*image;
 	int					w, h;
 	_pel				init;
 	gimp_image_t		*new;
@@ -153,13 +60,14 @@ static int glue_load_image(ClientData foo, Tcl_Interp *interp, //{{{1
 	if (objc < 2 || objc > 3)
 		CHECK_ARGS(1, "filename ?progress_callback?");
 
-	image = imlib_load_image_with_error_return(Tcl_GetString(objv[1]), &error);
-	if (error != IMLIB_LOAD_ERROR_NONE)
-		THROW_ERROR("Cannot load image", Tcl_GetString(objv[1]), ": ", 
-				lookup_load_error(error));
+	strncpy(g_imageinfo->filename, Tcl_GetString(objv[1]), MaxTextExtent-1);
+	g_imageinfo->filename[MaxTextExtent-1] = 0;
+	image = ReadImage(g_imageinfo, &g_exception);
+	//if (g_exception.severity != UndefinedException)
+		//CatchException(&g_exception);
+	if (image == (Image *)NULL)
+		THROW_ERROR("Cannot load image ", Tcl_GetString(objv[1]), ": ", g_exception.reason, g_exception.reason);
 
-	imlib_context_set_image(image);
-	
 	if (objc == 3) {
 		if (g_progress_cb != NULL) {
 			Tcl_DecrRefCount(g_progress_cb);
@@ -168,26 +76,29 @@ static int glue_load_image(ClientData foo, Tcl_Interp *interp, //{{{1
 		g_interp = interp;
 		g_progress_cb = objv[2];
 		Tcl_IncrRefCount(g_progress_cb);
-		imlib_context_set_progress_function(progress_func);
-		imlib_context_set_progress_granularity(10);
+		// TODO: Register progress callback
 	}
 
-	imlib_image_set_changes_on_disk();
-	w = imlib_image_get_width();
-	h = imlib_image_get_height();
+	w = image->columns;
+	h = image->rows;
+	DBG("Read image %p, w: %d, h: %d\n", image, w, h);
 	init.c = 0x00000000;
 
 	new = pmap_new(w, h, init);
 
-	memcpy(new->pixel_data, imlib_image_get_data_for_reading_only(), w * h * 4);
-
-	imlib_free_image();
+	DispatchImage(image, 0, 0, w, h, "BGRA", 0,
+			new->pixel_data, &g_exception);
+	if (g_exception.severity != UndefinedException) {
+		//CatchException(&g_exception);
+		THROW_ERROR("Cannot load image ", Tcl_GetString(objv[1]), ": ", g_exception.reason, g_exception.reason);
+	}
+	DestroyImage(image);
 
 	if (g_progress_cb != NULL) {
 		Tcl_DecrRefCount(g_progress_cb);
 		g_interp = NULL;
 		g_progress_cb = NULL;
-		imlib_context_set_progress_function(NULL);
+		// TODO: Deregister process callback
 	}
 	
 	Tcl_SetObjResult(interp, Tcl_NewPMAPObj(new));
@@ -196,14 +107,14 @@ static int glue_load_image(ClientData foo, Tcl_Interp *interp, //{{{1
 }
 
 
+/*
 static int glue_save_image(ClientData foo, Tcl_Interp *interp, //{{{1
 		int objc, Tcl_Obj *CONST objv[])
 {
-	Imlib_Image			image;
-	Imlib_Load_Error	error;
-	gimp_image_t		*pmap;
-	char				*filename;
-	char				*type;
+	Image			image;
+	gimp_image_t	*pmap;
+	char			*filename;
+	char			*type;
 	
 	if (objc < 3 || objc > 5)
 		CHECK_ARGS(2, "pmap filename ?type? ?progress_callback?");
@@ -257,78 +168,115 @@ static int glue_save_image(ClientData foo, Tcl_Interp *interp, //{{{1
 	return TCL_OK;
 }
 
-
+*/
 static int glue_scale_pmap(ClientData foo, Tcl_Interp *interp, //{{{1
 		int objc, Tcl_Obj *CONST objv[])
 {
 	gimp_image_t		*src_pmap;
 	gimp_image_t		*dest_pmap;
-	Imlib_Border		border;
-	Imlib_Image			*src;
-	Imlib_Image			*dest;
+	Image				*src;
+	Image				*dest;
 	_pel				init;
-	int					anti_alias = 1;
-	int					w, h;
+	int					w, h, filter, index;
+	double				blur;
+
+	static CONST char	*filters[] = {
+		"bessel", "blackman", "box", "catrom", "cubic", "gaussian", 
+		"hanning", "hermite", "lanczos", "mitchell", "point", "quadratic",
+		"sinc", "triangle", (char *)NULL
+	};
+	enum {
+		F_BESSEL, F_BLACKMAN, F_BOX, F_CATROM, F_CUBIC, F_GAUSSIAN,
+		F_HANNING, F_HERMITE, F_LANCZOS, F_MITCHELL, F_POINT, F_QUADRATIC,
+		F_SINC, F_TRIANGLE
+	};
 	
-	if (objc < 4 || (objc > 6 && objc != 9 && objc != 10))
-		CHECK_ARGS(3, "src_pmap w h ?smooth? ?border_l border_r border_t border_b? ?progress_callback?");
+	if (objc < 4 || objc > 6)
+		CHECK_ARGS(3, "src_pmap w h ?filter? ?blur?");
 
 	TEST_OK(Tcl_GetPMAPFromObj(interp, objv[1], &src_pmap));
-
-	src = imlib_create_image_using_data(src_pmap->width, src_pmap->height,
-			(DATA32 *)src_pmap->pixel_data);
-	if (src == NULL)
-		THROW_ERROR("Failed to wrap src_pmap in an imlib2 image");
-
-	imlib_context_set_image(src);
-	
 	TEST_OK(Tcl_GetIntFromObj(interp, objv[2], &w));
 	TEST_OK(Tcl_GetIntFromObj(interp, objv[3], &h));
-	if (objc >= 5)
-		TEST_OK(Tcl_GetBooleanFromObj(interp, objv[4], &anti_alias));
-	if (objc == 9) {
-		TEST_OK(Tcl_GetIntFromObj(interp, objv[5], &border.left));
-		TEST_OK(Tcl_GetIntFromObj(interp, objv[6], &border.right));
-		TEST_OK(Tcl_GetIntFromObj(interp, objv[7], &border.top));
-		TEST_OK(Tcl_GetIntFromObj(interp, objv[8], &border.bottom));
-		imlib_image_set_border(&border);
+	if (objc >= 5) {
+		TEST_OK(Tcl_GetIndexFromObj(interp, objv[4], (char **)filters, "filter",
+					0, &index));
+		switch (index) { //{{{
+			case F_BESSEL:
+				filter = BesselFilter;
+				break;
+			case F_BLACKMAN:
+				filter = BlackmanFilter;
+				break;
+			case F_BOX:
+				filter = BoxFilter;
+				break;
+			case F_CATROM:
+				filter = CatromFilter;
+				break;
+			case F_CUBIC:
+				filter = CubicFilter;
+				break;
+			case F_GAUSSIAN:
+				filter = GaussianFilter;
+				break;
+			case F_HANNING:
+				filter = HanningFilter;
+				break;
+			case F_HERMITE:
+				filter = HermiteFilter;
+				break;
+			case F_LANCZOS:
+				filter = LanczosFilter;
+				break;
+			case F_MITCHELL:
+				filter = MitchellFilter;
+				break;
+			case F_POINT:
+				filter = PointFilter;
+				break;
+			case F_QUADRATIC:
+				filter = QuadraticFilter;
+				break;
+			case F_SINC:
+				filter = SincFilter;
+				break;
+			case F_TRIANGLE:
+				filter = TriangleFilter;
+				break;
+			default:
+				THROW_ERROR("Error fetching filter lookup, unrecognised filter");
+				break;
+		} //}}}
+	} else {
+		filter = LanczosFilter;
+	}
+	if (objc >= 6) {
+		TEST_OK(Tcl_GetDoubleFromObj(interp, objv[5], &blur));
+	} else {
+		blur = 1.0;
 	}
 
-	if (objc == 10) {
-		if (g_progress_cb != NULL) {
-			Tcl_DecrRefCount(g_progress_cb);
-			g_progress_cb = NULL;
-		}
-		g_interp = interp;
-		g_progress_cb = objv[9];
-		Tcl_IncrRefCount(g_progress_cb);
-		imlib_context_set_progress_function(progress_func);
-		imlib_context_set_progress_granularity(10);
-	}
-	
-	imlib_context_set_anti_alias((char)anti_alias);
+	src = ConstituteImage(src_pmap->width, src_pmap->height,
+			"BGRA", CharPixel, src_pmap->pixel_data, &g_exception);
+	if (src == NULL)
+		THROW_ERROR("Failed to wrap src_pmap in an imagemagick image",
+				g_exception.description);
 
-	dest = imlib_create_cropped_scaled_image(0, 0,
-			src_pmap->width, src_pmap->height,
-			w, h);
-	imlib_free_image();
-
-	if (g_progress_cb != NULL) {
-		Tcl_DecrRefCount(g_progress_cb);
-		g_interp = NULL;
-		g_progress_cb = NULL;
-		imlib_context_set_progress_function(NULL);
-	}
+	dest = ResizeImage(src, w, h, filter, blur, &g_exception);
+	DestroyImage(src);
 
 	if (dest == NULL)
 		THROW_ERROR("Failed to scale image");
 
-	imlib_context_set_image(dest);
 	init.c = 0x00000000;
 	dest_pmap = pmap_new(w, h, init);
-	memcpy(dest_pmap->pixel_data,
-			imlib_image_get_data_for_reading_only(), w * h * 4);
-	imlib_free_image();
+	DispatchImage(dest, 0, 0, w, h, "BGRA", 0,
+			dest_pmap->pixel_data, &g_exception);
+	if (g_exception.severity != UndefinedException) {
+		//CatchException(&g_exception);
+		THROW_ERROR("Cannot export imagemagic image to pmap: ", g_exception.description, g_exception.reason);
+	}
+	DestroyImage(dest);
 
 	Tcl_SetObjResult(interp, Tcl_NewPMAPObj(dest_pmap));
 
@@ -336,18 +284,14 @@ static int glue_scale_pmap(ClientData foo, Tcl_Interp *interp, //{{{1
 }
 	
 
-int Pixel_imlib_Init(Tcl_Interp *interp) //{{{1
+int Pixel_magick_Init(Tcl_Interp *interp) //{{{1
 {
-	imlib_set_color_usage(128);
-
-	NEW_CMD("pixel::imlib2::set_cache_size", glue_set_cache_size);
-	NEW_CMD("pixel::imlib2::set_font_cache_size", glue_set_font_cache_size);
-	NEW_CMD("pixel::imlib2::add_path_to_font_path", glue_add_path_to_font_path);
-	NEW_CMD("pixel::imlib2::set_color_usage", glue_set_color_usage);
-	NEW_CMD("pixel::imlib2::context_set_dither", glue_context_set_dither);
-	NEW_CMD("pixel::imlib2::load_image", glue_load_image);
-	NEW_CMD("pixel::imlib2::save_image", glue_save_image);
-	NEW_CMD("pixel::imlib2::scale_pmap", glue_scale_pmap);
+	g_imageinfo = CloneImageInfo((ImageInfo *)NULL);
+	GetExceptionInfo(&g_exception);
+	
+	NEW_CMD("pixel::magick::load_image", glue_load_image);
+//	NEW_CMD("pixel::magick::save_image", glue_save_image);
+	NEW_CMD("pixel::magick::scale_pmap", glue_scale_pmap);
 	
 	return TCL_OK;
 }
