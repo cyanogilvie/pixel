d92e5a7495d1581a8526c83d6eca947c550f77d1
cvs <cvs>
2004-05-15 23:38:52 +0000

    One header to bring them all and in the darkness bind them

---
+++ all.h
@@ -0,0 +1,20 @@
+#include "2d.h"
+#include "bezierstuff.h"
+#include "misc.h"
+#include "primitives.h"
+#include "shaders.h"
+#include "tcl_pmap.h"
+#include "ttf.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <string.h>
+#include <math.h>
+#include <signal.h>
+#include <tcl.h>
+#include <tclstuff.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <wchar.h>
+#include <locale.h>
