86e2b3a94ac714bd5ee7af7611c939e4386a1394
Cyan Ogilvie <cyan@tirion.(none)>
2007-07-31 12:32:48 +0200

    Refactored core package into a tea extension, and factored out the ttf stuff into its own tea extension

--- pkgIndex.tcl
+++
@@ -1,5 +0,0 @@
-#Tcl package index file, version 1.0
-
-package ifneeded Pixel 2.7 [list eval [list namespace eval ::pixel [list variable library $dir]]\n[list source [file join $dir pixel.tcl]]]
-
-
