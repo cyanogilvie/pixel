4331297ace429de7ca7f897a479b10322b0188f6
Cyan Ogilvie <cyan@tirion.codeforge.co.za>
2008-06-16 10:47:16 +0200

    Added scaling wrapper

--- pixel_devil/generic/main.c
+++ pixel_devil/generic/main.c
@@ -1,3 +1,5 @@
+// vim: foldmethod=marker foldmarker=<<<,>>> ts=4 shiftwidth=4
+//
 #include <tclstuff.h>
 #include <Pixel/2d.h>
 #include <Pixel/tcl_pmap.h>
@@ -7,7 +9,7 @@
 //#include <IL/ilut.h>
 
 
-static int glue_load_image(cdata, interp, objc, objv)
+static int glue_load_image(cdata, interp, objc, objv) //<<<
 	ClientData		cdata;
 	Tcl_Interp		*interp;
 	int				objc;
@@ -58,8 +60,8 @@ static int glue_load_image(cdata, interp, objc, objv)
 	return TCL_OK;
 }
 
-
-static int glue_load_image_from_var(cdata, interp, objc, objv)
+//>>>
+static int glue_load_image_from_var(cdata, interp, objc, objv) //<<<
 	ClientData		cdata;
 	Tcl_Interp		*interp;
 	int				objc;
@@ -113,8 +115,81 @@ static int glue_load_image_from_var(cdata, interp, objc, objv)
 	return TCL_OK;
 }
 
+//>>>
+static int glue_scale_pmap(cdata, interp, objc, objv) //<<<
+	ClientData		cdata;
+	Tcl_Interp		*interp;
+	int				objc;
+	Tcl_Obj *CONST	objv[];
+{
+	ILuint				id;
+	_pel				init;
+	gimp_image_t		*src;
+	gimp_image_t		*new;
+	int					new_w, new_h, index;
+	ILenum				filter;		// unsigned int
+	static CONST char *filters[] = {
+		"ILU_NEAREST",
+		"ILU_LINEAR",
+		"ILU_BILINEAR",
+		"ILU_SCALE_BOX",
+		"ILU_SCALE_TRIANGLE",
+		"ILU_SCALE_BELL",
+		"ILU_SCALE_BSPLINE",
+		"ILU_SCALE_LANCZOS3",
+		"ILU_SCALE_MITCHELL",
+		(char *)NULL
+	};
+	ILenum map[] = {
+		ILU_NEAREST,
+		ILU_LINEAR,
+		ILU_BILINEAR,
+		ILU_SCALE_BOX,
+		ILU_SCALE_TRIANGLE,
+		ILU_SCALE_BELL,
+		ILU_SCALE_BSPLINE,
+		ILU_SCALE_LANCZOS3,
+		ILU_SCALE_MITCHELL
+	};
 
-int Pixel_devil_Init(Tcl_Interp *interp)
+	if (objc < 4 || objc > 5) {
+		CHECK_ARGS(3, "src_pmap new_width new_height ?filter?");
+	}
+
+	TEST_OK(Tcl_GetPMAPFromObj(interp, objv[1], &src));
+	TEST_OK(Tcl_GetIntFromObj(interp, objv[2], &new_w));
+	TEST_OK(Tcl_GetIntFromObj(interp, objv[3], &new_h));
+	if (objc == 5) {
+		TEST_OK(Tcl_GetIndexFromObj(interp, objv[4], filters, "filter",
+					TCL_EXACT, &index));
+		filter = map[index];
+	} else {
+		filter = ILU_SCALE_LANCZOS3;	// Good but slow
+	}
+
+	ilGenImages(1, &id);
+	ilBindImage(id);
+
+	ilTexImage(src->width, src->height, 1, 4, IL_BGRA, IL_UNSIGNED_BYTE,
+			src->pixel_data);
+	iluImageParameter(ILU_FILTER, filter);
+	iluScale(new_w, new_h, 1);
+
+	init.c = 0;
+	new = pmap_new(new_w, new_h, init);
+
+	ilCopyPixels(0, 0, 0, new_w, new_h, 1, IL_BGRA, IL_UNSIGNED_BYTE,
+			new->pixel_data);
+
+	ilDeleteImages(1, &id);
+
+	Tcl_SetObjResult(interp, Tcl_NewPMAPObj(new));
+
+	return TCL_OK;
+}
+
+//>>>
+int Pixel_devil_Init(Tcl_Interp *interp) //<<<
 {
 	if (Tcl_InitStubs(interp, "8.1", 0) == NULL) {
 		return TCL_ERROR;
@@ -125,8 +200,9 @@ int Pixel_devil_Init(Tcl_Interp *interp)
 
 	NEW_CMD("pixel::devil::load_image", glue_load_image);
 	NEW_CMD("pixel::devil::load_image_from_var", glue_load_image_from_var);
+	NEW_CMD("pixel::devil::scale_pmap", glue_scale_pmap);
 
 	return TCL_OK;
 }
 
-
+//>>>
