ed06a26e9fcb6e7435f64a67a62b9ad592ee8367
Cyan Ogilvie <cyan@tirion.codeforge.co.za>
2008-06-19 11:41:30 +0200

    Fixed missing sudo in libPixelstub3.a

--- pixel_core/build_linux.sh
+++ pixel_core/build_linux.sh
@@ -8,7 +8,7 @@ make || exit 1
 make genstubs || exit 1
 make binaries || exit 1
 
-cp libPixelstub3.a /tcl8.5/lib
+sudo cp libPixelstub3.a /tcl8.5/lib
 echo "Meta platform   linux-glibc2.6-ix86" >> teapot.txt
 teapot-pkg generate --type zip --output teapot .
 sudo cp generic/pixel.h /usr/include/Pixel
