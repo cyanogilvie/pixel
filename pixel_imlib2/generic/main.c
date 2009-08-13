4d618cecdc29c819464f4caf0068885db94dc5c1
Cyan Ogilvie <cyan@tirion.(none)>
2009-08-13 11:12:55 +0200

    Eeek, need to find a better way

--- pixel_imlib2/generic/main.c
+++ pixel_imlib2/generic/main.c
@@ -409,10 +409,12 @@ static int glue_sharpen_pmap(ClientData foo, Tcl_Interp *interp, //{{{1
 }
 	
 
-int Pixel_imlib_Init(Tcl_Interp *interp) //{{{1
+int Pixel_imlib2_Init(Tcl_Interp *interp) //{{{1
 {
-	if (Tcl_InitStubs(interp, "8.1", 0) == NULL)
-		return TCL_ERROR;
+	if (Tcl_InitStubs(interp, "8.1", 0) == NULL) return TCL_ERROR;
+#ifdef USE_PIXEL_STUBS
+	if (Pixel_InitStubs(interp, "3.4", 0) == NULL) return TCL_ERROR;
+#endif
 
 	imlib_set_color_usage(128);
 	imlib_set_cache_size(0);
@@ -429,7 +431,9 @@ int Pixel_imlib_Init(Tcl_Interp *interp) //{{{1
 	NEW_CMD("pixel::imlib2::scale_pmap", glue_scale_pmap);
 	NEW_CMD("pixel::imlib2::blur", glue_blur_pmap);
 	NEW_CMD("pixel::imlib2::sharpen", glue_sharpen_pmap);
-	
+
+	TEST_OK(Tcl_PkgProvide(interp, PACKAGE_NAME, PACKAGE_VERSION));
+
 	return TCL_OK;
 }
 
