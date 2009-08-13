77052a0fea8c5fe5a82b77b97620ea248a454285
Cyan Ogilvie <cyan@tirion.(none)>
2009-08-13 11:09:25 +0200

    Fixes

--- pixel_fonts/fonts.tcl
+++ pixel_fonts/fonts.tcl
@@ -3,6 +3,9 @@ package require Pixel
 namespace eval ::pixel::fonts {
 	namespace export *
 
+	variable library \
+			[file normalize [file join [pwd] [file dirname [info script]]]]
+
 	variable version 0.1
 	set hold	[pwd]
 	cd $::pixel::fonts::library
--- pixel_fonts/pkgIndex.tcl
+++ pixel_fonts/pkgIndex.tcl
@@ -1,4 +1,4 @@
 #Tcl package index file, version 1.2
 
-package ifneeded Pixel_fonts 0.1 [list eval [list namespace eval ::pixel::fonts [list variable library $dir]]\n[list source [file join $dir fonts.tcl]]]
+package ifneeded Pixel_fonts 0.1 [list source [file join $dir init.tcl]]
 
