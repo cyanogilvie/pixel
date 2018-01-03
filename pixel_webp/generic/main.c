fdff88e0aad18ec5ca62f6d279c8c1fcc9701754
cyan <cyan.ogilvie@gmail.com>
2018-01-03 23:07:43 +0200

    Add webp encode and decode support (only alpha channel, lossy support for now)

---
+++ pixel_webp/generic/main.c
@@ -0,0 +1,68 @@
+#include <stdint.h>
+#include <webp/encode.h>
+#include <webp/decode.h>
+#include "tclstuff.h"
+#include <pixel.h>
+
+
+static int glue_encodewebp(ClientData* cdata, Tcl_Interp* interp, int objc, Tcl_Obj *const objv[]) //{{{
+{
+	gimp_image_t*	pmap;
+	int				quality;
+	size_t			length;
+	uint8_t*		webp_data;
+	
+	CHECK_ARGS(2, "pmap quality");
+
+	TEST_OK(Tcl_GetPMAPFromObj(interp, objv[1], &pmap));
+	TEST_OK(Tcl_GetIntFromObj(interp, objv[2], &quality));
+
+	length = WebPEncodeBGRA((uint8_t*)pmap->pixel_data, pmap->width, pmap->height, pmap->width*4, quality, &webp_data);
+
+	Tcl_SetObjResult(interp, Tcl_NewByteArrayObj(webp_data, length));
+
+	free(webp_data);
+	
+	return TCL_OK;
+}
+
+//}}}
+static int glue_decodewebp(ClientData* cdata, Tcl_Interp* interp, int objc, Tcl_Obj *const objv[]) //{{{
+{
+	uint8_t*		webp_data;
+	int				length;
+	gimp_image_t*	new;
+	
+	CHECK_ARGS(1, "webp_data");
+
+	webp_data = Tcl_GetByteArrayFromObj(objv[1], &length);
+
+	new = (gimp_image_t*)malloc(sizeof(gimp_image_t));
+	new->bytes_per_pixel = 4;
+	new->pixel_data = (_pel*)WebPDecodeBGRA(webp_data, length, (int*)&new->width, (int*)&new->height);
+	if (new == NULL)
+		THROW_ERROR("Error loading webp");
+
+	Tcl_SetObjResult(interp, Tcl_NewPMAPObj(new));
+
+	return TCL_OK;
+}
+
+//}}}
+int Pixel_webp_Init(Tcl_Interp* interp) //{{{
+{
+	if (Tcl_InitStubs(interp, "8.4", 0) == NULL) return TCL_ERROR;
+	if (Pixel_InitStubs(interp, "3.3", 0) == NULL) return TCL_ERROR;
+
+	NEW_CMD("pixel::webp::encode", glue_encodewebp);
+	NEW_CMD("pixel::webp::decode", glue_decodewebp);
+
+	if (Tcl_PkgProvide(interp, PACKAGE_NAME, PACKAGE_VERSION) != TCL_OK)
+		return TCL_ERROR;
+
+	return TCL_OK;
+}
+
+//}}}
+
+// vim: foldmethod=marker foldmarker={{{,}}}
---
+++ pixel_webp/generic/tclstuff.h
@@ -0,0 +1,61 @@
+// Written by Cyan Ogilvie, and placed in the public domain
+
+#ifndef _TCLSTUFF_H
+#define _TCLSTUFF_H
+
+#include "tcl.h"
+
+#define NEW_CMD( tcl_cmd, c_cmd ) \
+	Tcl_CreateObjCommand( interp, tcl_cmd, \
+			(Tcl_ObjCmdProc *) c_cmd, \
+			(ClientData *) NULL, NULL );
+
+#define THROW_ERROR( ... )								\
+	{													\
+		Tcl_AppendResult(interp, ##__VA_ARGS__, NULL);	\
+		return TCL_ERROR;								\
+	}
+
+#define THROW_ERROR_LABEL( label, var, ... )				\
+	{														\
+		Tcl_AppendResult(interp, ##__VA_ARGS__, NULL);		\
+		var = TCL_ERROR;									\
+		goto label;											\
+	}
+
+// convenience macro to check the number of arguments passed to a function
+// implementing a tcl command against the number expected, and to throw
+// a tcl error if they don't match.  Note that the value of expected does
+// not include the objv[0] object (the function itself)
+#define CHECK_ARGS(expected, msg)										\
+	if (objc != expected + 1) {											\
+		Tcl_ResetResult( interp );										\
+		Tcl_AppendResult( interp, "Wrong # of arguments.  Must be \"",	\
+						  msg, "\"", NULL );							\
+		return TCL_ERROR;												\
+	}
+
+
+// A rather frivolous macro that just enhances readability for a common case
+#ifdef DEBUG
+#warning Using DEBUG version of TEST_OK
+#define TEST_OK( cmd )		\
+	if (cmd != TCL_OK) { \
+		fprintf( stderr, "TEST_OK() returned TCL_ERROR\n" ); \
+		return TCL_ERROR; \
+	}
+#define TEST_OK_LABEL( label, var, cmd )		\
+	if (cmd != TCL_OK) { \
+		var = TCL_ERROR; \
+		goto label; \
+	}
+#else
+#define TEST_OK( cmd )		\
+	if (cmd != TCL_OK) return TCL_ERROR;
+#endif
+#define TEST_OK_LABEL( label, var, cmd )		\
+	if (cmd != TCL_OK) { \
+		var = TCL_ERROR; \
+		goto label; \
+	}
+#endif
