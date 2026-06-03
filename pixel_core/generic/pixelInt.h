b56224624618d0312f016bbd7db9c7b335449fea
Cyan Ogilvie <cyan.ogilvie@gmail.com>
2026-06-03 09:44:49 -0300

    Provide fallback for `unreachable()`

--- pixel_core/generic/pixelInt.h
+++ pixel_core/generic/pixelInt.h
@@ -8,4 +8,14 @@
 
 #include <pixel.h>
 
+#if !HAVE_UNREACHABLE
+# if defined(__GNUC__) || defined(__clang__)
+#  define unreachable() __builtin_unreachable()
+# elif defined(_MSC_VER)
+#  define unreachable() __assume(0)
+# else
+#  define unreachable() ((void)0)
+# endif
+#endif
+
 #endif
